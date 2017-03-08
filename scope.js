3ecb17b3f487994e2f63f4983cf3100ca308455f
CaMnter <yuanyu.camnter@gmail.com>
2017-03-08 23:24:52 +0800

    [Add] scope.js in root path

---
+++ scope.js
@@ -0,0 +1,50 @@
+/**
+ * Created by：CaMnter
+ */
+
+var v = 'Save';
+var o10 = {
+    v: 'Save - o10',
+    getV: function () {
+        return this.v;
+    }
+};
+console.log(o10.getV());
+
+var o11 = {
+    v: 'Save - o11',
+    getV: function () {
+        this.v = 'Save - o11 - getV';
+        return this.v;
+    }
+};
+console.log(o11.getV());
+
+function f1() {
+    this.v = 'Save - f1';
+    return function f11() {
+        this.v = 'Save - f11';
+        return this.v;
+    }
+}
+console.log(f1()());
+
+let v_this = this;
+function f2() {
+    var _this = this;
+    this.v = 'Save - f2';
+    return function f21() {
+        this.v = 'Save - f21';
+        console.log(this === _this);
+        console.log(this === v_this);
+        return _this.v;
+    }
+}
+console.log(f2()());
+function f4() {
+    console.log(this.v);
+}
+var o13 = {
+    v: "o13 - v"
+};
+f4.bind(o13)();

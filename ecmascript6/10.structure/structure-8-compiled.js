b693b264073f1e2f8f515a8fd6d1e02c541b9035
CaMnter <yuanyu.camnter@gmail.com>
2017-04-02 22:50:41 +0800

    [Add] structure-8 in ecmascript6 module

---
+++ ecmascript6/10.structure/structure-8-compiled.js
@@ -0,0 +1,135 @@
+"use strict";
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+/**
+ * Created by：CaMnter
+ */
+
+/***************
+ * ES6 WeakMap *
+ ***************/
+
+/**
+ * WeakMap 只接受对象作为 key，除开 null
+ * 键名所指向的对象，不计入垃圾回收机制
+ */
+
+(function () {
+    var weakMap = new WeakMap();
+    try {
+        weakMap.set(2, 233);
+    } catch (e) {
+        console.log("[structure]  [test-" + 1 + "]  [e] = ", e, '\n');
+    }
+    try {
+        weakMap.set(Symbol('save'), 233);
+    } catch (e) {
+        console.log("[structure]  [test-" + 1 + "]  [e] = ", e, '\n');
+    }
+})();
+
+/**
+ * WeakMap 的设计目的在于，键名是对象的弱引用（垃圾回收机制不将该引用考虑在内）
+ * 所以其所对应的对象可能会被自动回收
+ * 当对象被回收后，WeakMap 自动移除对应的键值对
+ *
+ * 一个对应 DOM 元素的 WeakMap 结构
+ * 当某个 DOM 元素被清除，其所对应的 WeakMap 记录就会自动被移除
+ * 基本上，WeakMap 的专用场合就是，它的键所对应的对象，可能会在将来消失
+ *
+ * WeakMap 结构有助于防止内存泄漏
+ */
+(function () {
+    var objectKey = {};
+    var weakMap = new WeakMap();
+
+    weakMap.set(objectKey, 233);
+    console.log("[structure]  [test-" + 2 + "]  [weakMap.get(objectKey)] = ", weakMap.get(objectKey));
+    objectKey = null;
+    // 找不到了
+    console.log("[structure]  [test-" + 2 + "]  [weakMap.get(objectKey)] = ", weakMap.get(objectKey), '\n');
+})();
+
+/**
+ * WeakMap 与 Map 在 API 上的区别主要是两个：
+ * 一：是没有遍历操作。没有 key()、values()、entries()
+ * 二：无法清空，即不支持 clear 方法
+ *
+ * 因为 WeakMap 的键不被计入引用、被垃圾回收机制忽略有关
+ * 所以，WeakMap 只有四个方法：get()、set()、has()、delete()
+ */
+(function () {
+    var objectKey = {};
+    var weakMap = new WeakMap();
+    weakMap.set(objectKey, 233);
+    console.log("[structure]  [test-" + 3 + "]  [weakMap.key] = ", weakMap.key);
+    console.log("[structure]  [test-" + 3 + "]  [weakMap.size] = ", weakMap.size);
+    console.log("[structure]  [test-" + 3 + "]  [weakMap.values] = ", weakMap.values);
+    console.log("[structure]  [test-" + 3 + "]  [weakMap.entries] = ", weakMap.entries);
+})();
+
+/**
+ * WeakMap 应用的典型场合就是 DOM 节点作为键名
+ *
+ * 每当发生 click 事件，就更新一下状态
+ * 当这个 DOM 节点被移除时，相应的状态也会被移除
+ */
+// (() => {
+//     let element = document.getElementById('banner');
+//     let weakMap = new WeakMap();
+//
+//     weakMap.set(element, {timesClicked: 0});
+//
+//     element.addEventListener('click', function () {
+//         let bannerData = weakMap.get(element);
+//         bannerData.timesClicked++;
+//     }, false);
+// })();
+
+/**
+ * 作用：私有属性
+ */
+(function () {
+    var _counter = new WeakMap();
+    var _action = new WeakMap();
+
+    var Countdown = function () {
+        function Countdown(counter, action) {
+            _classCallCheck(this, Countdown);
+
+            _counter.set(this, counter);
+            _action.set(this, action);
+        }
+
+        _createClass(Countdown, [{
+            key: "dec",
+            value: function dec() {
+                var counter = _counter.get(this);
+                if (counter < 1) return;
+                counter--;
+                _counter.set(this, counter);
+                if (counter === 0) {
+                    _action.get(this)();
+                }
+            }
+        }]);
+
+        return Countdown;
+    }();
+
+    var LIMIT = 7;
+    var countdown = new Countdown(LIMIT, function () {
+        return console.log("[structure]  [test-" + 4 + "]  [Countdown#dec]   time out");
+    });
+    for (var i = 0; i < LIMIT; i++) {
+        countdown.dec();
+    }
+    countdown = null;
+    console.log("[structure]  [test-" + 4 + "]  [_counter.get(countdown)] = ", _counter.get(countdown));
+    console.log("[structure]  [test-" + 4 + "]  [_action.get(countdown)] = ", _action.get(countdown));
+})();
+
+//# sourceMappingURL=structure-8-compiled.js.map

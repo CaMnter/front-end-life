083af52be379f3141c8779191c7183623686703f
CaMnter <yuanyu.camnter@gmail.com>
2017-04-17 11:04:45 +0800

    [Update] generator-3 in ecmascript6 module

--- ecmascript6/14.generator/generator-3-compiled.js
+++ ecmascript6/14.generator/generator-3-compiled.js
@@ -105,14 +105,14 @@
     // 得到 3
     console.log("[generator]  [test-" + 2 + "]  [generatorA.next()] = ", generatorA.next());
     // 调用前暂停到 yield (x / 3)，调用后 返回 x / 3，然后暂停到 return
-    // x = 2 * (yield (v + 1))，yield (v + 1) = undefined，
+    // x = 2 * (yield (v + 1))，yield (v + 1) = undefined
     // x = 2 * undefined = NaN
     // x / 3 = NaN
     console.log("[generator]  [test-" + 2 + "]  [generatorA.next()] = ", generatorA.next());
     // 调用前暂停到 return，调用后 返回 v + x + y，return 调用后结束
-    // x = 2 * (yield (v + 1))，yield (v + 1) = undefined，
+    // x = 2 * (yield (v + 1))，yield (v + 1) = undefined
     // x = 2 * undefined = NaN
-    // y = yield (x / 3)，yield (x / 3) = undefined，
+    // y = yield (x / 3)，yield (x / 3) = undefined
     // y = undefined
     // v + x + y = 2 + NaN + undefined = NaN
     console.log("[generator]  [test-" + 2 + "]  [generatorA.next()] = ", generatorA.next());
@@ -122,17 +122,158 @@
     // 得到 3
     console.log("[generator]  [test-" + 3 + "]  [generatorB.next()] = ", generatorB.next());
     // 调用前暂停到 yield (x / 3)，调用后 返回 x / 3，然后暂停到 return
-    // x = 2 * (yield (v + 1))，yield (v + 1) = 3，
+    // x = 2 * (yield (v + 1))，yield (v + 1) = 3
     // x = 2 * 3 = 6
     // x / 3 = 2
     console.log("[generator]  [test-" + 3 + "]  [generatorB.next()] = ", generatorB.next(3));
     // 调用前暂停到 return，调用后 返回 v + x + y，return 调用后结束
-    // x = 2 * (yield (v + 1))，yield (v + 1) = 3，
+    // x = 2 * (yield (v + 1))，yield (v + 1) = 3
     // x = 2 * 3 = 6
-    // y = yield (x / 3)，yield (x / 3) = 3，
+    // y = yield (x / 3)，yield (x / 3) = 3
     // y = 3
     // v + x + y = 2 + 6 + 3 = 11
-    console.log("[generator]  [test-" + 3 + "]  [generatorB.next()] = ", generatorB.next(3));
+    console.log("[generator]  [test-" + 3 + "]  [generatorB.next()] = ", generatorB.next(3), '\n');
+})();
+
+/**
+ * 第一次 next 的参数值是失效的
+ *
+ * 以上面的为例：yield (v + 1)，调用后 返回 v + 1，完全不涉及到 yield (v + 1) 的值，只关心了 yield
+ * 后面的 v + 1 的值
+ *
+ * 如果想要第一调用 next 方法时，参数值生效
+ * 需要在 Generator 包一层
+ *
+ * 实质上就是完成了：
+ * 创建 Generator 方法 + 手动调用一次 next
+ * 然后这个参数作为 创建 Generator 方法 时的初始值
+ */
+(function () {
+    function wrapperGenerator(generatorFunction) {
+        return function () {
+            var generator = generatorFunction.apply(undefined, arguments);
+            generator.next();
+            return generator;
+        };
+    }
+
+    var wrapped = wrapperGenerator(regeneratorRuntime.mark(function _callee() {
+        return regeneratorRuntime.wrap(function _callee$(_context3) {
+            while (1) {
+                switch (_context3.prev = _context3.next) {
+                    case 0:
+                        _context3.t0 = console;
+                        _context3.t1 = "[generator]  [test-" + 4 + "]  [yield] = ";
+                        _context3.next = 4;
+                        return;
+
+                    case 4:
+                        _context3.t2 = _context3.sent;
+
+                        _context3.t0.log.call(_context3.t0, _context3.t1, _context3.t2);
+
+                        return _context3.abrupt("return", "done");
+
+                    case 7:
+                    case "end":
+                        return _context3.stop();
+                }
+            }
+        }, _callee, this);
+    }));
+
+    var generator = wrapped();
+    generator.next('Save');
+    console.log("[generator]  [test-" + 4 + "]  [generator.next()] = ", generator.next(), '\n');
+})();
+
+/**
+ * 向 Generator 输入值的例子
+ */
+(function () {
+    var _marked3 = [generatorFunction].map(regeneratorRuntime.mark);
+
+    function generatorFunction() {
+        return regeneratorRuntime.wrap(function generatorFunction$(_context4) {
+            while (1) {
+                switch (_context4.prev = _context4.next) {
+                    case 0:
+                        console.log("[generator]  [test-" + 5 + "]  [start]");
+                        _context4.t0 = console;
+                        _context4.t1 = "[generator]  [test-" + 5 + "]  [ 1. yield ] = ";
+                        _context4.next = 5;
+                        return;
+
+                    case 5:
+                        _context4.t2 = _context4.sent;
+
+                        _context4.t0.log.call(_context4.t0, _context4.t1, _context4.t2);
+
+                        _context4.t3 = console;
+                        _context4.t4 = "[generator]  [test-" + 5 + "]  [ 2. yield ] = ";
+                        _context4.next = 11;
+                        return;
+
+                    case 11:
+                        _context4.t5 = _context4.sent;
+
+                        _context4.t3.log.call(_context4.t3, _context4.t4, _context4.t5);
+
+                        _context4.t6 = console;
+                        _context4.t7 = "[generator]  [test-" + 5 + "]  [ 3. yield ] = ";
+                        _context4.next = 17;
+                        return;
+
+                    case 17:
+                        _context4.t8 = _context4.sent;
+
+                        _context4.t6.log.call(_context4.t6, _context4.t7, _context4.t8);
+
+                        _context4.t9 = console;
+                        _context4.t10 = "[generator]  [test-" + 5 + "]  [ 4. yield ] = ";
+                        _context4.next = 23;
+                        return;
+
+                    case 23:
+                        _context4.t11 = _context4.sent;
+
+                        _context4.t9.log.call(_context4.t9, _context4.t10, _context4.t11);
+
+                    case 25:
+                    case "end":
+                        return _context4.stop();
+                }
+            }
+        }, _marked3[0], this);
+    }
+
+    var generator = generatorFunction();
+    generator.next();
+    var save = 'Save';
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+        for (var _iterator = save[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var char = _step.value;
+
+            generator.next(char);
+        }
+    } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+    } finally {
+        try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+                _iterator.return();
+            }
+        } finally {
+            if (_didIteratorError) {
+                throw _iteratorError;
+            }
+        }
+    }
 })();
 
 //# sourceMappingURL=generator-3-compiled.js.map
--- ecmascript6/14.generator/generator-3.js
+++ ecmascript6/14.generator/generator-3.js
@@ -50,14 +50,14 @@
     // 得到 3
     console.log("[generator]  [test-" + 2 + "]  [generatorA.next()] = ", generatorA.next());
     // 调用前暂停到 yield (x / 3)，调用后 返回 x / 3，然后暂停到 return
-    // x = 2 * (yield (v + 1))，yield (v + 1) = undefined，
+    // x = 2 * (yield (v + 1))，yield (v + 1) = undefined
     // x = 2 * undefined = NaN
     // x / 3 = NaN
     console.log("[generator]  [test-" + 2 + "]  [generatorA.next()] = ", generatorA.next());
     // 调用前暂停到 return，调用后 返回 v + x + y，return 调用后结束
-    // x = 2 * (yield (v + 1))，yield (v + 1) = undefined，
+    // x = 2 * (yield (v + 1))，yield (v + 1) = undefined
     // x = 2 * undefined = NaN
-    // y = yield (x / 3)，yield (x / 3) = undefined，
+    // y = yield (x / 3)，yield (x / 3) = undefined
     // y = undefined
     // v + x + y = 2 + NaN + undefined = NaN
     console.log("[generator]  [test-" + 2 + "]  [generatorA.next()] = ", generatorA.next());
@@ -67,15 +67,67 @@
     // 得到 3
     console.log("[generator]  [test-" + 3 + "]  [generatorB.next()] = ", generatorB.next());
     // 调用前暂停到 yield (x / 3)，调用后 返回 x / 3，然后暂停到 return
-    // x = 2 * (yield (v + 1))，yield (v + 1) = 3，
+    // x = 2 * (yield (v + 1))，yield (v + 1) = 3
     // x = 2 * 3 = 6
     // x / 3 = 2
     console.log("[generator]  [test-" + 3 + "]  [generatorB.next()] = ", generatorB.next(3));
     // 调用前暂停到 return，调用后 返回 v + x + y，return 调用后结束
-    // x = 2 * (yield (v + 1))，yield (v + 1) = 3，
+    // x = 2 * (yield (v + 1))，yield (v + 1) = 3
     // x = 2 * 3 = 6
-    // y = yield (x / 3)，yield (x / 3) = 3，
+    // y = yield (x / 3)，yield (x / 3) = 3
     // y = 3
     // v + x + y = 2 + 6 + 3 = 11
-    console.log("[generator]  [test-" + 3 + "]  [generatorB.next()] = ", generatorB.next(3));
+    console.log("[generator]  [test-" + 3 + "]  [generatorB.next()] = ", generatorB.next(3), '\n');
+})();
+
+/**
+ * 第一次 next 的参数值是失效的
+ *
+ * 以上面的为例：yield (v + 1)，调用后 返回 v + 1，完全不涉及到 yield (v + 1) 的值，只关心了 yield
+ * 后面的 v + 1 的值
+ *
+ * 如果想要第一调用 next 方法时，参数值生效
+ * 需要在 Generator 包一层
+ *
+ * 实质上就是完成了：
+ * 创建 Generator 方法 + 手动调用一次 next
+ * 然后这个参数作为 创建 Generator 方法 时的初始值
+ */
+(() => {
+    function wrapperGenerator(generatorFunction) {
+        return function (...args) {
+            let generator = generatorFunction(...args);
+            generator.next();
+            return generator;
+        };
+    }
+
+    const wrapped = wrapperGenerator(function*() {
+        console.log("[generator]  [test-" + 4 + "]  [yield] = ", yield);
+        return "done";
+    });
+
+    let generator = wrapped();
+    generator.next('Save');
+    console.log("[generator]  [test-" + 4 + "]  [generator.next()] = ", generator.next(), '\n');
+})();
+
+/**
+ * 向 Generator 输入值的例子
+ */
+(() => {
+    function * generatorFunction() {
+        console.log("[generator]  [test-" + 5 + "]  [start]");
+        console.log("[generator]  [test-" + 5 + "]  [ 1. yield ] = ", yield);
+        console.log("[generator]  [test-" + 5 + "]  [ 2. yield ] = ", yield);
+        console.log("[generator]  [test-" + 5 + "]  [ 3. yield ] = ", yield);
+        console.log("[generator]  [test-" + 5 + "]  [ 4. yield ] = ", yield);
+    }
+
+    let generator = generatorFunction();
+    generator.next();
+    let save = 'Save';
+    for (let char of save) {
+        generator.next(char);
+    }
 })();

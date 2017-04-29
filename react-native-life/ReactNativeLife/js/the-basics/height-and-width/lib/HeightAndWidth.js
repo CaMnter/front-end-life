b3323e341fe564d30a4eca6311b28d20e0472534
CaMnter <yuanyu.camnter@gmail.com>
2017-04-29 22:50:59 +0800

    [Modify] the style in HeightAndWidth.js in js module

--- react-native-life/ReactNativeLife/js/the-basics/height-and-width/lib/HeightAndWidth.js
+++ react-native-life/ReactNativeLife/js/the-basics/height-and-width/lib/HeightAndWidth.js
@@ -20,10 +20,10 @@ class FixedDimensionsView extends Component {
 class FlexView extends Component {
     render() {
         return (
-            <View style={{flex: 233}}>
+            <View style={{width: 233, height: 233}}>
                 <View style={{flex: 1, backgroundColor: 'powderblue'}}/>
-                <View style={{flex: 4, backgroundColor: 'skyblue'}}/>
-                <View style={{flex: 8, backgroundColor: 'steelblue'}}/>
+                <View style={{flex: 2, backgroundColor: 'skyblue'}}/>
+                <View style={{flex: 3, backgroundColor: 'steelblue'}}/>
             </View>
         );
     }

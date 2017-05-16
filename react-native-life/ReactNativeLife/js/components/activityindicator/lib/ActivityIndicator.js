2a281b8a0c4213a23c81f94f1277f6be72e81286
CaMnter <yuanyu.camnter@gmail.com>
2017-05-16 23:47:50 +0800

    [Update] ActivityIndicator.js in react-native-life module

--- react-native-life/ReactNativeLife/js/components/activityindicator/lib/ActivityIndicator.js
+++ react-native-life/ReactNativeLife/js/components/activityindicator/lib/ActivityIndicator.js
@@ -3,7 +3,12 @@
  */
 
 import React, {Component} from 'react';
-import {ActivityIndicator, StyleSheet, View} from 'react-native';
+import {
+    ActivityIndicator,
+    ScrollView,
+    StyleSheet,
+    View
+} from 'react-native';
 
 type State = { animating: boolean; };
 type Timer = number;
@@ -11,7 +16,7 @@ type Timer = number;
 class ActivityIndicatorView extends Component {
 
     state: State;
-    _time: Timer;
+    _timer: Timer;
 
     constructor(props) {
         super(props);
@@ -22,8 +27,8 @@ class ActivityIndicatorView extends Component {
 
 
     setToggleTimeout() {
-        this._time = setTimeout(() => {
-            this.state({animating: !this.state.animating});
+        this._timer = setTimeout(() => {
+            this.setState({animating: !this.state.animating});
             this.setToggleTimeout();
         }, 2000);
     }
@@ -34,16 +39,57 @@ class ActivityIndicatorView extends Component {
 
 
     componentWillUnmount() {
-        clearTimeout(this._time);
+        clearTimeout(this._timer);
     }
 
 
     render() {
         return (
-            <ActivityIndicator
-                animating={this.state.animating}
-                style={[styles.centering, {height: 67}]}
-                size="large"/>
+            <ScrollView>
+                <ActivityIndicator
+                    animating={this.state.animating}
+                    style={[styles.centering, {height: 67}]}
+                    size="large"/>
+                <ActivityIndicator
+                    style={[styles.centering, styles.gray]}
+                    color="white"/>
+                <View>
+                    <ActivityIndicator
+                        style={[styles.centering]}/>
+                    <ActivityIndicator
+                        style={[styles.centering, {backgroundColor: '#eeeeee'}]}/>
+                </View>
+                <View style={styles.horizontal}>
+                    <ActivityIndicator color="#0000ff"/>
+                    <ActivityIndicator color="#aa00aa"/>
+                    <ActivityIndicator color="#aa3300"/>
+                    <ActivityIndicator color="#00aa00"/>
+                </View>
+                <ActivityIndicator
+                    style={[styles.centering, styles.gray]}
+                    size="large"
+                    color="white"/>
+                <View style={styles.horizontal}>
+                    <ActivityIndicator
+                        size="large"
+                        color="#0000ff"/>
+                    <ActivityIndicator
+                        size="large"
+                        color="#aa00aa"/>
+                    <ActivityIndicator
+                        size="large"
+                        color="#aa3300"/>
+                    <ActivityIndicator
+                        size="large"
+                        color="#00aa00"/>
+                </View>
+                <ActivityIndicator
+                    style={[styles.centering, {transform: [{scale: 1.5}]}]}
+                    size="large"/>
+                <ActivityIndicator
+                    style={styles.centering}
+                    size={75}/>
+            </ScrollView>
         );
     }
 
@@ -55,118 +101,118 @@ const ZActivityIndicator = {
 
 module.exports = ZActivityIndicator;
 
-// exports.displayName = (undefined: ?string);
-// exports.framework = 'React';
+exports.displayName = (undefined: ?string);
+exports.framework = 'React';
 // exports.title = '<ActivityIndicator>';
-// exports.description = 'Animated loading indicators.';
-//
-// exports.examples = [
-//     {
-//         title: 'Default (small, white)',
-//         render() {
-//             return (
-//                 <ActivityIndicator
-//                     style={[styles.centering, styles.gray]}
-//                     color="white"
-//                 />
-//             );
-//         }
-//     },
-//     {
-//         title: 'Gray',
-//         render() {
-//             return (
-//                 <View>
-//                     <ActivityIndicator
-//                         style={[styles.centering]}
-//                     />
-//                     <ActivityIndicator
-//                         style={[styles.centering, {backgroundColor: '#eeeeee'}]}
-//                     />
-//                 </View>
-//             );
-//         }
-//     },
-//     {
-//         title: 'Custom colors',
-//         render() {
-//             return (
-//                 <View style={styles.horizontal}>
-//                     <ActivityIndicator color="#0000ff"/>
-//                     <ActivityIndicator color="#aa00aa"/>
-//                     <ActivityIndicator color="#aa3300"/>
-//                     <ActivityIndicator color="#00aa00"/>
-//                 </View>
-//             );
-//         }
-//     },
-//     {
-//         title: 'Large',
-//         render() {
-//             return (
-//                 <ActivityIndicator
-//                     style={[styles.centering, styles.gray]}
-//                     size="large"
-//                     color="white"
-//                 />
-//             );
-//         }
-//     },
-//     {
-//         title: 'Large, custom colors',
-//         render() {
-//             return (
-//                 <View style={styles.horizontal}>
-//                     <ActivityIndicator
-//                         size="large"
-//                         color="#0000ff"
-//                     />
-//                     <ActivityIndicator
-//                         size="large"
-//                         color="#aa00aa"
-//                     />
-//                     <ActivityIndicator
-//                         size="large"
-//                         color="#aa3300"
-//                     />
-//                     <ActivityIndicator
-//                         size="large"
-//                         color="#00aa00"
-//                     />
-//                 </View>
-//             );
-//         }
-//     },
-//     {
-//         title: 'Start/stop',
-//         render() {
-//             return <ToggleAnimatingActivityIndicator />;
-//         }
-//     },
-//     {
-//         title: 'Custom size',
-//         render() {
-//             return (
-//                 <ActivityIndicator
-//                     style={[styles.centering, {transform: [{scale: 1.5}]}]}
-//                     size="large"
-//                 />
-//             );
-//         }
-//     },
-//     {
-//         platform: 'android',
-//         title: 'Custom size (size: 75)',
-//         render() {
-//             return (
-//                 <ActivityIndicator
-//                     style={styles.centering}
-//                     size={75}
-//                 />
-//             );
-//         }
-//     },
-// ];
+exports.description = 'Animated loading indicators.';
+
+exports.examples = [
+    {
+        title: 'Default (small, white)',
+        render() {
+            return (
+                <ActivityIndicator
+                    style={[styles.centering, styles.gray]}
+                    color="white"
+                />
+            );
+        }
+    },
+    {
+        title: 'Gray',
+        render() {
+            return (
+                <View>
+                    <ActivityIndicator
+                        style={[styles.centering]}
+                    />
+                    <ActivityIndicator
+                        style={[styles.centering, {backgroundColor: '#eeeeee'}]}
+                    />
+                </View>
+            );
+        }
+    },
+    {
+        title: 'Custom colors',
+        render() {
+            return (
+                <View style={styles.horizontal}>
+                    <ActivityIndicator color="#0000ff"/>
+                    <ActivityIndicator color="#aa00aa"/>
+                    <ActivityIndicator color="#aa3300"/>
+                    <ActivityIndicator color="#00aa00"/>
+                </View>
+            );
+        }
+    },
+    {
+        title: 'Large',
+        render() {
+            return (
+                <ActivityIndicator
+                    style={[styles.centering, styles.gray]}
+                    size="large"
+                    color="white"
+                />
+            );
+        }
+    },
+    {
+        title: 'Large, custom colors',
+        render() {
+            return (
+                <View style={styles.horizontal}>
+                    <ActivityIndicator
+                        size="large"
+                        color="#0000ff"
+                    />
+                    <ActivityIndicator
+                        size="large"
+                        color="#aa00aa"
+                    />
+                    <ActivityIndicator
+                        size="large"
+                        color="#aa3300"
+                    />
+                    <ActivityIndicator
+                        size="large"
+                        color="#00aa00"
+                    />
+                </View>
+            );
+        }
+    },
+    {
+        title: 'Start/stop',
+        render() {
+            // return <ToggleAnimatingActivityIndicator />;
+        }
+    },
+    {
+        title: 'Custom size',
+        render() {
+            return (
+                <ActivityIndicator
+                    style={[styles.centering, {transform: [{scale: 1.5}]}]}
+                    size="large"
+                />
+            );
+        }
+    },
+    {
+        platform: 'android',
+        title: 'Custom size (size: 75)',
+        render() {
+            return (
+                <ActivityIndicator
+                    style={styles.centering}
+                    size={75}
+                />
+            );
+        }
+    },
+];
 
 
 const styles = StyleSheet.create({

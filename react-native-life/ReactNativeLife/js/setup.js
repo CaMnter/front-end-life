c3fcd810329d22f483467f0175798caba3251af1
CaMnter <yuanyu.camnter@gmail.com>
2017-06-03 02:13:52 +0800

    [Add] ViewExample.js in react-native-life module

--- react-native-life/ReactNativeLife/js/setup.js
+++ react-native-life/ReactNativeLife/js/setup.js
@@ -61,7 +61,8 @@ import React from 'react';
 // import Root from "./Components/StatusBar/index"
 // import Root from "./Components/Text/index"
 // import Root from "./Components/TextInput/index"
-import Root from "./Components/ToolbarAndroid/index"
+// import Root from "./Components/ToolbarAndroid/index"
+import Root from "./Components/View/index"
 
 
 function setup(): ReactClass<{}> {

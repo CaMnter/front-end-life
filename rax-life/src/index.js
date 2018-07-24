21b7125cdfea12327359fb318a9c8c4717c687f9
CaMnter <yuanyu.camnter@gmail.com>
2018-07-24 23:13:19 +0800

    [Add] TouchComponent

--- rax-life/src/index.js
+++ rax-life/src/index.js
@@ -2,5 +2,6 @@ import { createElement, render } from 'rax';
 import App from './js/hello';
 import { PropsComponent } from './js/props';
 import StateComponent from './js/state';
+import TouchComponent from './js/touch';
 
-render(<StateComponent />);
+render(<TouchComponent />);

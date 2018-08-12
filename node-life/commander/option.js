5d916d066946f629055f6d72fca6874f0825f6fa
CaMnter <yuanyu.camnter@gmail.com>
2018-08-12 23:05:03 +0800

    [Add] option.js

---
+++ node-life/commander/option.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+/**
+ * @author CaMnter
+ */
+
+var program = require('commander');
+
+program
+  .version('0.1.0')
+  .option('-c, --cm', '「CaMnter」')
+  // [] 会默认加上在 program 上
+  .option('-c, --save [save]', '「Save you from anything」', 'save')
+  .option('--no-cm', 'no cm')
+  .parse(process.argv);
+
+console.log('option:');
+if (program.cm) console.log('  - cm');
+else console.log('warning: without cm ');
+if (program.save) console.log('  - save');
+
+
+// node commander/option --cm
+// node commander/option --save

fe250b6de424302bcd33075f78790f5051b17f7e
CaMnter <yuanyu.camnter@gmail.com>
2018-08-26 19:59:42 +0800

    [Add] download.js

---
+++ node-life/lib/download.js
@@ -0,0 +1,34 @@
+/**
+ * @author CaMnter
+ */
+
+const ora      = require('ora'); // https://github.com/sindresorhus/ora
+const path     = require('path');
+const download = require('download-git-repo');
+
+
+const downloadFromUrl = (target, type = 'github', url) => {
+  let targetUrl = '';
+  switch (type) {
+    case 'github':
+      targetUrl = `https://github.com/${url}`;
+      break;
+  }
+
+  target = path.join(target || '.', '.download-temp');
+
+  return new Promise((resolve, reject) => {
+    const spinner = ora(`正在下载项目模板，源地址：${targetUrl}`);
+    spinner.start();
+    download(url, target, { clone: true }, err => {
+      if (err) {
+        spinner.fail();
+        console.log('err', err);
+        reject(err)
+      } else {
+        spinner.succeed();
+        resolve(target)
+      }
+    })
+  });
+};

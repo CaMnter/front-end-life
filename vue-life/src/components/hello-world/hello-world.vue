b4f1a31c7092071ce44c3dee0152bdb44aa55d95
CaMnter <yuanyu.camnter@gmail.com>
2018-12-08 21:12:13 +0800

    「Refactor」

---
+++ vue-life/src/components/hello-world/hello-world.vue
@@ -0,0 +1,20 @@
+<style scoped>
+    @import './hello-world.css';
+</style>
+
+<template>
+    <div id='root'>
+        {{ message }}
+        <text>2333</text>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'HelloWorld',
+        props: {
+            message: String
+        }
+    }
+</script>

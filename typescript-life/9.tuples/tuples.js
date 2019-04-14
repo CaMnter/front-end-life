e81e7adbe174d4892c394713fc83377da67b4d50
CaMnter <yuanyu.camnter@gmail.com>
2019-04-14 19:03:47 +0800

    「Add」tuples

---
+++ typescript-life/9.tuples/tuples.js
@@ -0,0 +1,6 @@
+"use strict";
+/**
+ * Created by：CaMnter
+ */
+var t1 = ['CaMnter', 7];
+console.log("t1 : " + t1);
---
+++ typescript-life/9.tuples/tuples.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by：CaMnter
+ */
+
+const t1: [string, number] = ['CaMnter', 7];
+console.log(`t1 : ${ t1 }`);
